a97171195334ae80c12c9b745ac31dafcaff98a6
aygulmardanova <mardanovaaigul@mail.ru>
2016-05-28 23:19:01 +0300

    New method in RestApiController for class adding

--- FitnessClub/src/main/java/ru/kpfu/itis/aygul/controller/RestAPIController.java
+++ FitnessClub/src/main/java/ru/kpfu/itis/aygul/controller/RestAPIController.java
@@ -1,11 +1,18 @@
 package ru.kpfu.itis.aygul.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import ru.kpfu.itis.aygul.model.ClassEntity;
 import ru.kpfu.itis.aygul.model.User;
+import ru.kpfu.itis.aygul.model.api.ClassClient;
 import ru.kpfu.itis.aygul.model.api.UserClient;
+import ru.kpfu.itis.aygul.service.interfaces.ClassService;
 import ru.kpfu.itis.aygul.service.interfaces.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by aygulmardanova on 27.05.16.
@@ -17,22 +24,45 @@ public class RestAPIController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    ClassService classService;
+
     @RequestMapping(value = "/{login}", method = RequestMethod.GET)
     @ResponseBody
     public UserClient login(ModelMap model, @PathVariable String login) {
 
         User user = userService.getUserByLogin(login);
-        System.out.println("User = " + user);
+        System.out.println("API User = " + user);
 
         if (user != null) {
             UserClient client = new UserClient(user);
-            System.out.println(client);
             return client;
         } else {
             return null;
         }
     }
 
+    @RequestMapping(value = "/classes", method = RequestMethod.GET)
+    @ResponseBody
+    public List<ClassClient> getClasses() {
+        List<ClassEntity> classEntityList = classService.getAll();
+        List<ClassClient> classes = new ArrayList<>();
+        for (ClassEntity classEntity : classEntityList) {
+            classes.add(new ClassClient(classEntity));
+        }
+        return classes;
+    }
 
+    @RequestMapping(value = "add_class", method = RequestMethod.POST)
+    @ResponseBody
+    public Boolean addClass(ModelMap model, @RequestParam String name, @RequestParam String description) {
+
+        if (classService.getClassByName(name) != null) {
+            return false;
+        } else {
+            classService.addClass(name, description, null);
+        }
+        return true;
+    }
 
 }
